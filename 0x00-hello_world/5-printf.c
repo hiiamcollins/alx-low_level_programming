ad46fd41ce7a27c1f540fb1739d36c43cfb3bd31
hiiamcollins <alphacollins8@gmail.com>
2023-07-14 09:06:17 -0700

    task 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ * main - a c program that uses printf statement to print "ith proper grammar, but the outcome is a piece of art,
+ * Return - always return (0)
+ */
+int main(void)
+{
+	printf("\"with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
