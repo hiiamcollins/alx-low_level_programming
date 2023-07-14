d704a89c5670f4e681588cddcc3a3be473b696d8
hiiamcollins <alphacollins8@gmail.com>
2023-07-14 08:37:09 -0700

    files

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/*
+ * main - print a statement without using printf statemen
+ * Return - return always (0)
+ */
+int main (void)
+{
+	puts("Programming is like building a multilingual puzzle\n");
+	return (0);
+}
